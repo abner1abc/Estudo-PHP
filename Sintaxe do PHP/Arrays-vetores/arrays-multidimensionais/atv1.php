b3fb8632d09061bc348e9fb30b209fd0886718fa
Abner Valverde Cunha <abnercunha2247@gmail.com>
2024-07-31 05:14:01 -0300

    add new files

---
+++ Sintaxe do PHP/Arrays-vetores/arrays-multidimensionais/atv1.php
@@ -0,0 +1,16 @@
+<?php
+    $frutas = array (
+		"vermelhas" => array( 
+		    "melancia", 
+		    "cereja", 
+		    "framboesa",
+		    "morango"
+		 ),    
+ 		
+		"citricas" => array(
+		    "laranja", 
+		    "limao",
+		    "abacaxi",
+		    "mexerica"
+		 ),
+    );
